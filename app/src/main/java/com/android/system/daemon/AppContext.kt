6dfb9ba2dc55d822203dfe8df0b3e13f1cf5b3a4
YuXiul <2422271946@qq.com>
2026-04-22 12:54:29 +0800

    Delete AppContext.kt and its functionality
    Remove AppContext object and its disableSelf function.

--- app/src/main/java/com/android/system/daemon/AppContext.kt
+++ app/src/main/java/com/android/system/daemon/AppContext.kt
@@ -1,16 +1,1 @@
 package com.android.system.daemon
-
-import android.content.ComponentName
-import android.content.Context
-import android.content.pm.PackageManager
-
-object AppContext {
-    fun disableSelf(context: Context) {
-        val component = ComponentName(context, MainActivity::class.java)
-        context.packageManager.setComponentEnabledSetting(
-            component,
-            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-            PackageManager.DONT_KILL_APP
-        )
-    }
-}
