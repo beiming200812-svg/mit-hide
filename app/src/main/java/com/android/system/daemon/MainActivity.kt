475912b56a2b6de347ea3a9ba524129caf70eccc
YuXiul <2422271946@qq.com>
2026-04-22 13:16:24 +0800

    Update MainActivity.kt

--- app/src/main/java/com/android/system/daemon/MainActivity.kt
+++ app/src/main/java/com/android/system/daemon/MainActivity.kt
@@ -2,20 +2,11 @@ package com.android.system.daemon
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_SECURE or
-            WindowManager.LayoutParams.FLAG_EXCLUDE_FROM_RECENTS,
-            WindowManager.LayoutParams.FLAG_SECURE or
-            WindowManager.LayoutParams.FLAG_EXCLUDE_FROM_RECENTS
-        )
-
         setContentView(R.layout.activity_main)
         startService(Intent(this, QuietService::class.java))
     }
