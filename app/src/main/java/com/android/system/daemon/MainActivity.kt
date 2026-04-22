a13098e14940adb8dc8f2a91b21b2e47c6e5815e
YuXiul <2422271946@qq.com>
2026-04-22 12:01:10 +0800

    Add MainActivity with secure window flag

---
+++ app/src/main/java/com/android/system/daemon/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.android.system.daemon
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+        setContentView(R.layout.activity_main)
+    }
+}
