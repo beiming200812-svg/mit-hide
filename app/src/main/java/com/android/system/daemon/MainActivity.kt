c36562ca0ae9eb16c0307ad09c759a022781e7e1
YuXiul <2422271946@qq.com>
2026-04-22 13:48:26 +0800

    Implement process hiding in MainActivity
    Add method to lower thread priority and move task to back

--- app/src/main/java/com/android/system/daemon/MainActivity.kt
+++ app/src/main/java/com/android/system/daemon/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.android.system.daemon
 
 import android.os.Bundle
+import android.os.Process
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hideAppProcess()
+    }
+
+    private fun hideAppProcess() {
+        try {
+            Process.setThreadPriority(Process.THREAD_PRIORITY_LOWEST)
+        } catch (_: Exception) {}
+    }
+
+    override fun onBackPressed() {
+        moveTaskToBack(true)
     }
 }
